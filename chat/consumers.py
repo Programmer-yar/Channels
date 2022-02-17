8022f59acac7503bc8e681fa71146f5567539d0b
yar <ahmadyar228@gmail.com>
2022-02-17 23:03:00 +0500

    more clear understanding

--- chat/consumers.py
+++ chat/consumers.py
@@ -5,6 +5,13 @@ from channels.generic.websocket import AsyncWebsocketConsumer #WebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        """- self.scope
+        -> just like 'request' object
+        -> contains a lot of information including room_name,
+            user data, authentication data
+        -> in channels, middlewares add data to 'self.scope' the same way
+            middlewares add datat to 'request' object in django
+        """
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'chat_{self.room_name}' #'chat_%s' % self.room_name
 
@@ -14,6 +21,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.channel_name
             )
 
+        # if not called the connection will be rejected and closed
         await self.accept()
 
     async def disconnect(self, close_code):
@@ -23,9 +31,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
+    # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        print(message, self.room_name, self.room_group_name)
+        print(self.scope['user'].is_authenticated)
 
         # Send message to room group
         await self.channel_layer.group_send(
@@ -36,6 +47,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             }
         )
 
+    # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
 
